ded575f014f3dd5959e818eb1198e1ad5c668e61
Fangfang Xia <fangfang@gmail.com>
2013-09-30 12:28:29 -0500

    -c option for min_coverage

--- scripts/tab2fa.pl
+++ scripts/tab2fa.pl
@@ -3,18 +3,20 @@
 use strict;
 use Getopt::Long;
 
-my $usage = "Usage: $0 [ -m min_contig_length ] < contigs.fa > contigs.tab\n\n";
+my $usage = "Usage: $0 [ -m min_contig_length -c min_coverage ] < contigs.fa > contigs.tab\n\n";
 
-my ($help, $min_contig_length);
+my ($help, $min_contig_length, $min_coverage);
 
 GetOptions("h|help"  => \$help,
-           "m|min=i" => \$min_contig_length);
+           "m|min=i" => \$min_contig_length,
+           "c|cov=f" => \$min_coverage );
 
 $help and die $usage;  
 
 while (<STDIN>) {
     my ($id, $len, $cov, $stdev, $gc, $seed, $seq) = split /\t/;
     next if $len < $min_contig_length;
+    next if $cov < $min_coverage;
     print join("_", ">$id len", $len, 'cov', $cov, 'stdev', $stdev, 'GC', $gc, 'seed', $seed) . "\n";
     print $seq;
 }
